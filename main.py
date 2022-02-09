97463a75107e2ee2fe8cb3e6d06a2c4f64f1478c
Sheldonji <67721774+Sheldonji@users.noreply.github.com>
2022-02-09 00:09:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 22:36:20 2022
+
+@author: asus-pc
+"""
 #imports
 import os
 import tensorflow as tf
@@ -14,9 +20,9 @@ label_path = "F:/Image_data/data/label/" # label file path
 files = os.listdir(path)
 
 
+
 viewpoints = []
 filenames = []
-car_model = 0
 carmodel_list = []
 year_list = []
 car_makenames = []
@@ -31,23 +37,32 @@ for l in files:
         for j in files2:
             j = j + "/"
             files3  = os.listdir(path + l + k + j) 
-            car_model = car_model+1
-        
+    
             for i in files3:
                 filename, _ = i.split(".")
                 content = open(label_path + l + k + j + filename + '.txt').read()
-                if (content[0] != "3"):
+                if (content[0] != "3") & (content[0] != "-"):
                     filenames.append(filename)
                     viewpoints.append(content[0])
-                    carmodel_list.append(car_model)
                     year,_ = j.split("/")
                     year_list.append(year)
                     car_make,_ = l.split("/")
                     car_makenames.append(car_make)
+                    car_model,_ = k.split("/")
+                    carmodel_list.append(car_model)
         
         
-d = {'View Point': viewpoints, 'Filename': filenames, 'CarModel': cartype_list, 'Year': year_list, 'CarMake': car_makenames}
+d = {'View Point': viewpoints, 'Filename': filenames, 'CarModel': carmodel_list, 'Year': year_list, 'CarMake': car_makenames}
 df = pd.DataFrame(data=d)
 
+
+df_front = df[df['View Point'] == "1"]
+df_front_side = df[df['View Point'] == "4"]
+df_front_all = pd.concat([df_front, df_front_side])
+df_rear = df[df['View Point'] == "2"]
+df_rear_side = df[df['View Point'] == "5"]
+df_rear_all = pd.concat([df_rear, df_rear_side])
+print(df_front_all)
+print(df_rear_all)
 print(df)
-print(df.loc[df['CarModel'] == 1])
+
